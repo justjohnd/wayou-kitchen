1c4320e6794a46cb76802cedc41b4b7a4e3bc9f0
johnd <justjohnd@gmail.com>
2021-12-21 10:57:02 +0900

    remove const from create fetch and add function to add instructions to new Recipe after setDataArray

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -7,100 +7,90 @@ import InstructionsInputs from './instructionsInputs';
 import axios from 'axios';
 
 export default function Create() {
+
   const [newRecipe, setNewRecipe] = useState({
-      title: '',
-      preparationMinutes: '',
-      cookingMinutes: '',
-      analyzedInstructions: []
+    title: '',
+    preparationMinutes: '',
+    cookingMinutes: '',
+    analyzedInstructions: [],
   });
 
   // data and dataArray contain instructions data
-    const [data, setData] = useState('');
-    const [dataArray, setDataArray] = useState([]);
-
-    let analyzedInstructionsValues = [];
+  const [data, setData] = useState('');
+  const [dataArray, setDataArray] = useState([]);
 
   //These functions use callbacks from instructionsInputs to set data and dataArray for instructions information
+
+  // Any time dataArray is changed, instructions are updated in NewRecipe
+  function AddInstructionToRecipe() {
+    setNewRecipe((prevValue) => {
+      return {
+        ...prevValue,
+        analyzedInstructions: dataArray.map((data, index) => ({
+          number: index,
+          step: data,
+        })),
+      };
+    });
+  }
+
   function handleInstructionCallback(e) {
     setData(e.target.value);
   }
 
   function addInstructionCallback() {
     setDataArray((prevVal) => [...prevVal, data]);
-
-    setNewRecipe((prevValue) => {
-      return {
-        ...prevValue,
-        analyzedInstructions: dataArray.map((data, index) => ({
-            number: index,
-            step: data,
-        }))
-      };
-    });
-
+    AddInstructionToRecipe();
     setData('');
   }
 
   function editInstructionCallback(index, value) {
-          setDataArray((prevVal) => {
-            const newArray = [...prevVal];
-            newArray.splice(index, 1, value);
-            return newArray;
-          });
+    setDataArray((prevVal) => {
+      const newArray = [...prevVal];
+      newArray.splice(index, 1, value);
+      return newArray;
+    });
+    AddInstructionToRecipe();
   }
 
   function deleteInstructionCallback(id) {
-          setDataArray((prevItems) => {
-            return prevItems.filter((item, index) => {
-              return index !== id;
-            });
-          });
+    setDataArray((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+    AddInstructionToRecipe();
   }
 
   function insertInstructionCallback(idx) {
-        setDataArray((prevVal) => {
-          const newArray = [...prevVal];
-          newArray.splice(idx, 0, '');
-          return newArray;
-        });
-  }
-
-  function AddInstructionToRecipe() {
-    // const analyzedInstructionsValues = instructions.map((instruction, index) => {
-    //   return {
-    //     number: index,
-    //     step: instruction
-    //   };
-    // });
+    setDataArray((prevVal) => {
+      const newArray = [...prevVal];
+      newArray.splice(idx, 0, '');
+      return newArray;
+    });
+    AddInstructionToRecipe();
   }
 
   function handleData(e) {
     const { name, value } = e.target;
 
-    setNewRecipe(prevValue => {
+    setNewRecipe((prevValue) => {
       return {
         ...prevValue,
-        [name]: value
+        [name]: value,
       };
     });
   }
 
   let navigate = useNavigate();
 
-  // // This function will handle the submission.
+  // This function will handle the submission.
   function handleAddRecipe(e) {
     e.preventDefault();
 
-  //   // When post request is sent to the create url, axios will add a new record(newrecipe) to the database.
-    const newperson = {
-      title: newRecipe.title,
-      preparationMinutes: newRecipe.preparationMinutes,
-      cookingMinutes: newRecipe.cookingMinutes,
-      analyzedInstructions: newRecipe.analyzedInstructions
-    };
-
+    // When post request is sent to the create url, axios will add a new record to the database.
     axios
-      .post('http://localhost:5000/record/add', newperson)
+      .post('http://localhost:5000/record/add', newRecipe)
       .then((res) => console.log(res.data));
 
     // We will empty the state after posting the data to the database
@@ -113,62 +103,60 @@ export default function Create() {
     navigate('/');
   }
 
-  console.log(newRecipe);
-
   // This following section will display the form that takes the input from the user.
   // render() {
-    return (
-      <div style={{ marginTop: 20 }}>
-        <h3>Create New Record</h3>
-        <form onSubmit={handleAddRecipe}>
-          <div className="form-group">
-            <label>Name of the recipe: </label>
-            <input
-              name="title"
-              type="text"
-              className="form-control"
-              value={newRecipe.title}
-              onChange={(e) => handleData(e)}
-            />
-          </div>
-          <InstructionsInputs
-          data = {data}
-          dataArray = {dataArray}
-          handleInstructionCallback = {handleInstructionCallback}
-          addInstructionCallback= {addInstructionCallback}
+  return (
+    <div style={{ marginTop: 20 }}>
+      <h3>Create New Record</h3>
+      <form onSubmit={handleAddRecipe}>
+        <div className="form-group">
+          <label>Name of the recipe: </label>
+          <input
+            name="title"
+            type="text"
+            className="form-control"
+            value={newRecipe.title}
+            onChange={(e) => handleData(e)}
+          />
+        </div>
+        <InstructionsInputs
+          data={data}
+          dataArray={dataArray}
+          handleInstructionCallback={handleInstructionCallback}
+          addInstructionCallback={addInstructionCallback}
           editInstructionCallback={editInstructionCallback}
           deleteInstructionCallback={deleteInstructionCallback}
           insertInstructionCallback={insertInstructionCallback}
+        />
+        <div className="form-group">
+          <label>Preparation Minutes: </label>
+          <input
+            name="preparationMinutes"
+            type="text"
+            className="form-control"
+            value={newRecipe.preparationMinutes}
+            onChange={(e) => handleData(e)}
           />
-          <div className="form-group">
-            <label>Preparation Minutes: </label>
-            <input
-              name="preparationMinutes"
-              type="text"
-              className="form-control"
-              value={newRecipe.preparationMinutes}
-              onChange={(e) => handleData(e)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Cooking Minutes: </label>
-            <input
-              name="cookingMinutes"
-              type="text"
-              className="form-control"
-              value={newRecipe.cookingMinutes}
-              onChange={(e) => handleData(e)}
-            />
-          </div>
+        </div>
+        <div className="form-group">
+          <label>Cooking Minutes: </label>
+          <input
+            name="cookingMinutes"
+            type="text"
+            className="form-control"
+            value={newRecipe.cookingMinutes}
+            onChange={(e) => handleData(e)}
+          />
+        </div>
 
-          <div className="form-group">
-            <input
-              type="submit"
-              value="Create recipe"
-              className="btn btn-primary"
-            />
-          </div>
-        </form>
-      </div>
-    );
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create recipe"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
