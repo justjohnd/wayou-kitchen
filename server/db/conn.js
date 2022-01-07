bf7ae7605955932c523d1d8b5faff99e4e0e2fc9
johnd <justjohnd@gmail.com>
2022-01-07 11:33:04 +0900

    move mongoose to conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,25 +1,50 @@
-const { MongoClient } = require('mongodb');
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const { MongoClient } = require('mongodb');
+// const Db = process.env.ATLAS_URI;
+// const client = new MongoClient(Db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-var _db;
+// var _db;
+
+// module.exports = {
+//   connectToServer: function (callback) {
+//     client.connect(function (err, db) {
+//       // Verify we got a good "db" object
+//       if (db) {
+//         _db = db.db('myFirstDatabase');
+//         console.log('Successfully connected to MongoDB.');
+//       }
+//       return callback(err);
+//     });
+//   },
+
+//   getDb: function () {
+//     return _db;
+//   },
+// };
+const mongoose = require('mongoose');
+
+let _db;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
 
 module.exports = {
   connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
+    connection.once('open', (err, db) => {
       if (db) {
-        _db = db.db('myFirstDatabase');
+        _db = connection;
         console.log('Successfully connected to MongoDB.');
       }
       return callback(err);
     });
   },
-
-  getDb: function () {
-    return _db;
-  },
+    getDb: function () {
+      return connection;
+    },
 };
+
+
