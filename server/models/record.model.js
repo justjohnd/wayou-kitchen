e65ba67b172e1d5a3b414935ba7ebddc9e51ac49
johnd <justjohnd@gmail.com>
2022-01-07 10:21:38 +0900

    Add record model and test server connection with mongoose

---
+++ server/models/record.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const recordSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+    extendedIngredients: {
+    type: String,
+  },
+    preparationMinutes: {
+    type: String,
+  },
+    cookingMinutes: {
+    type: String,
+  },
+    readyInMinutes: {
+    type: String,
+  },
+    sourceUrl: {
+    type: String,
+  },
+    image: {
+    type: String,
+  },
+    analyzedInstructions: {
+    type: String,
+  },
+  servings: {
+    type: String,
+  },
+});
+
+const Record = mongoose.model('Record', recordSchema);
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config({ path: './config.env' });
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(require('./routes/record'));
 // get driver connection
 const dbo = require('./db/conn');
+
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('mongo DB success');
+// });
+
+// app.listen(port, (err) => {
+//   if (err) console.error(err);
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
 
 app.listen(port, () => {
   // perform a database connection when server starts
