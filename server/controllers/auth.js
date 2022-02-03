5bf5d60df072c5f805514cc538646392833141a7
johnd <justjohnd@gmail.com>
2022-02-03 19:02:36 +0900

    Reset password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
 const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 exports.register = async (req, res, next) => {
   const { userName, email, password } = req.body;
@@ -86,7 +87,32 @@ exports.forgotPassword = async (req, res, next) => {
 };
 
 exports.resetPassword = (req, res, next) => {
-  res.send("Reset Password Route");
+  const resetToken = crypto.createHash("sha256").update(req.params.resetToken).digest("hex");
+
+  try {
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() }
+    });
+
+    if (!user) {
+      return next(new ErrorResponse("Invalid Reset Token", 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPassowrdExpire = undefined;
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      data: "Password Reset Success"
+    });
+
+  } catch (error) {
+    next(error);
+  }
 };
 
 const sendToken = (user, statusCode, res) => {
