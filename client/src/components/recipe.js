601034dda34a6cd1be4fb000d1fa4fd04f63aa26
johnd <justjohnd@gmail.com>
2022-02-11 09:27:08 +0900

    Hide edit/delete functions from home page

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
 function Recipe(props) {
+
+console.log(props.privateScreen);
+
 return (
 <div className="recipe-home-container">
   {props.recordArray.map((currentrecord) => {
@@ -17,6 +20,7 @@ return (
           }
         />
         <div className="px-1 title">{currentrecord.title}</div>
+        {props.privateScreen ? (
           <div className="px-1">
             <Link to={'/show/' + currentrecord._id}>Show</Link> |
             <Link to={'/edit/' + currentrecord._id}>Edit</Link> |
@@ -29,6 +33,9 @@ return (
               Delete
             </a>
           </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     );
   })}
