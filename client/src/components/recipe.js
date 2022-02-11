c192f5f51d6fa728d1bcf7f835c9dac84916c807
johnd <justjohnd@gmail.com>
2022-02-11 09:31:43 +0900

    Add links to recipe images

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -11,18 +11,19 @@ return (
   {props.recordArray.map((currentrecord) => {
     return (
       <div key={uuidv4()} className="recipe-home">
-        <img
-          className="recipe-image mb-2 mx-2"
-          src={
-            currentrecord.image.slice(0, 4) === 'http'
-              ? currentrecord.image
-              : './images/' + currentrecord.image
-          }
-        />
-        <div className="px-1 title">{currentrecord.title}</div>
+        <Link to={'/show/' + currentrecord._id}>
+          <img
+            className="recipe-image mb-2 mx-2"
+            src={
+              currentrecord.image.slice(0, 4) === 'http'
+                ? currentrecord.image
+                : './images/' + currentrecord.image
+            }
+          />
+          <div className="px-1 title">{currentrecord.title}</div>
+        </Link>
         {props.privateScreen ? (
           <div className="px-1">
-            <Link to={'/show/' + currentrecord._id}>Show</Link> |
             <Link to={'/edit/' + currentrecord._id}>Edit</Link> |
             <a
               href="/"
