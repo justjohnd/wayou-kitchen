d4d7a04048f87ec293877b67f86ee48070fa5b19
johnd <justjohnd@gmail.com>
2022-03-04 13:11:27 +0900

    Move pagination to recipeList

--- server/client/src/components/recipeGroup.js
+++ server/client/src/components/recipeGroup.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Recipe from './recipe';
 
 function RecipeGroup(props) {
-
   let category;
   if (props.categoryRecords[0]) {
     category = props.categoryRecords[0].categories[0].value;
@@ -12,11 +11,15 @@ function RecipeGroup(props) {
   return (
     <div className="ms-3 mb-4">
       <h3>{props.categoryRecords.length > 0 && category}</h3>
-      <Recipe
-      recordArray={props.categoryRecords}
-      deleteRecord={props.deleteRecord}
-      privateScreen={props.privateScreen}
-      />
+      {props.categoryRecords && (
+        <div>
+        <Recipe
+          recordArray={props.categoryRecords}
+          deleteRecord={props.deleteRecord}
+          privateScreen={props.privateScreen}
+        />
+    </div>
+      )}
     </div>
   );
 }
--- server/client/src/components/screens/RecipeList.js
+++ server/client/src/components/screens/RecipeList.js
@@ -5,11 +5,35 @@ import Recipe from '../recipe';
 import RecipeGroup from '../recipeGroup';
 import CategoryDropdown from '../categoryDropdown';
 import useGetRecords from "../../hooks/useGetRecords";
+import Button from '../button';
 
 export default function RecipeList() {
   const records = useGetRecords("/record");
   const [selectedCategories, setSelectedCategories] = useState(null);
   const [recordCategories, setRecordCategories] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [postNumber] = useState(20);
+
+  const currentPageNumber = pageNumber * postNumber - postNumber;
+
+  const splicy = [...records];
+  const paginatedPosts = splicy.splice(currentPageNumber, postNumber);
+
+  const handlePrev = () => {
+    if (pageNumber === 1) return;
+    setPageNumber(pageNumber - 1);
+  };
+
+  const handleNext = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  // console.log('paginatedPosts: ', paginatedPosts);
+  // console.log('categoryRecords: ', records);
+  // console.log('pag length: ', paginatedPosts.length);
+  // console.log('currentPageNumber: ', currentPageNumber);
+  console.log(recordCategories);
+
 
   //Select by categories
   function categoriesCallback(optionSelected) {
@@ -37,7 +61,34 @@ export default function RecipeList() {
 
 function displayAll() {
   if (selectedCategories === null || selectedCategories.length === 0) {
-    return <Recipe recordArray={records} />;
+    return (
+      <div>
+        <Recipe recordArray={paginatedPosts} />
+        {paginatedPosts.length > 0 && (
+          <div className="pagination-wrapper">
+            <div className="d-flex justify-content-center">
+              Page {pageNumber}{' '}
+            </div>
+            <div className="d-flex">
+              <Button
+                buttonWrapper="w-50"
+                className="float-end me-2"
+                buttonText="Previous"
+                onClick={handlePrev}
+              />
+              {paginatedPosts.length > currentPageNumber && (
+                <Button
+                  buttonWrapper="w-50 text-left"
+                  className="float-start ms-2"
+                  buttonText="Next"
+                  onClick={handleNext}
+                />
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    );
   } 
 }
 
@@ -56,11 +107,13 @@ function displayAll() {
         recordCategories.map((categoryRecords, index) => {
           if (categoryRecords !== []) {
             return (
-              <RecipeGroup
-                key={uuidv4()}
-                index={index}
-                categoryRecords={categoryRecords}
-              />
+              <div>
+                <RecipeGroup
+                  key={uuidv4()}
+                  index={index}
+                  categoryRecords={categoryRecords}
+                />
+              </div>
             );
           }
         })
