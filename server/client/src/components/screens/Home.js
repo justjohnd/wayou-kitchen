5ab57fcf4316de23cf6d748cb0d262f3d2db7e25
johnd <justjohnd@gmail.com>
2022-07-25 11:08:37 +0900

    Add category dropdown logic to categoryDropdown component

--- server/client/src/components/screens/Home.js
+++ server/client/src/components/screens/Home.js
@@ -12,48 +12,6 @@ export default function Home({ loaderCallback }) {
   const [showAll, setShowAll] = useState(true);
   const [categorizedRecords, setCategorizedRecords] = useState(null);
 
-  //Select by categories. optionSelected argument is an array containing category objects
-  function categoriesCallback(optionSelected) {
-    //Put records in their on groups for display
-    const categoryTypes = optionSelected.map((category) => category.value);
-
-    if (categoryTypes.length > 0) {
-      setShowAll(false);
-    } else {
-      setShowAll(true);
-    }
-
-    // Filter out all records that match any of the categories selected (based on the records first category), and set state with that array
-    const groupArray = () => {
-      let newArray = [];
-      for (let i = 0; i < categoryTypes.length; i++) {
-        const group = records.filter((record) => {
-          // If index 0 is "other" but index 1 exists, categorized by index 1
-          let categories = record.categories;
-          if (categories[0].value === "other" && categories[1] !== undefined) {
-            if (categories[1].value === categoryTypes[i]) {
-              record.mainCat = categoryTypes[i];
-              return record;
-            }
-          }
-
-          if (categories[0].value === categoryTypes[i]) {
-            record.mainCat = categoryTypes[i];
-            return record;
-          }
-        });
-        if (group !== []) {
-          newArray.unshift(group);
-        }
-      }
-
-      return newArray;
-    };
-
-    const groupsToShow = groupArray();
-    setCategorizedRecords(groupsToShow);
-  }
-
   useEffect(() => {
     loaderCallback(true);
     setTimeout(() => {
@@ -65,7 +23,9 @@ export default function Home({ loaderCallback }) {
     <div className="p-3 container disable-while-loading">
       <div>
         <CategoryDropdown
-          categoriesCallback={categoriesCallback}
+          setShowAll={setShowAll}
+          setCategorizedRecords={setCategorizedRecords}
+          records={records}
         ></CategoryDropdown>
       </div>
 
